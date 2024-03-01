ee64f5af656c11f22961056cdf1bc47dd4ba39eb
PRAVIN <pk3419391@gmail.com>
2024-03-01 23:09:10 +0530

    commit

--- script.js
+++
@@ -1,8 +0,0 @@
-function tooggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    menu.classList.toggle("open");
-    
-    
-}
